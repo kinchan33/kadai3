af99902f86e11dd7a2c7e169488f2cba96b4c6c5
Masatoshi KANAMARU <a1520mk@aiit.ac.jp>
2015-06-08 23:06:53 +0900

    add test2

--- test.bash
+++ test.bash
@@ -7,7 +7,7 @@ error_exit() {
 }
 tmp=/tmp/$$
 #########
-# test
+# test 1
 #########
 # answer
 echo 9 > $tmp-ans
@@ -15,6 +15,17 @@ echo 9 > $tmp-ans
 ./15745120 45 27 > $tmp-out || error_exit "TEST1-1"
 # compare
 diff $tmp-ans $tmp-out || error_exit "TEST1-2"
+
+#########
+# test 2
+#########
+# answer
+echo 13 > $tmp-ans
+# execute program
+./15745120 52 39 > $tmp-out || error_exit "TEST2-1"
+# compare
+diff $tmp-ans $tmp-out || error_exit "TEST2-2"
+
 
 # success
 echo OK
