c092866eb3b2414a6cbac1603a8089d75b7a7467
Masatoshi KANAMARU <a1520mk@aiit.ac.jp>
2015-06-08 23:48:25 +0900

    add non-zero return in case of error

--- test.bash
+++ test.bash
@@ -15,6 +15,8 @@ echo 9 > $tmp-ans
 ./15745120 45 27 > $tmp-out || error_exit "TEST1-1"
 # compare
 diff $tmp-ans $tmp-out || error_exit "TEST1-2"
+# success
+echo TEST1 OK
 
 #########
 # test 2
@@ -25,9 +27,9 @@ echo 13 > $tmp-ans
 ./15745120 52 39 > $tmp-out || error_exit "TEST2-1"
 # compare
 diff $tmp-ans $tmp-out || error_exit "TEST2-2"
-
-
 # success
-echo OK
+echo TEST2 OK
+
+# remove temp file
 rm -f $tmp-*
 exit 0
